6d5076de8757d662633bf82448eb766fc2fe56f5
Vincenzo-00 <vince.vasta18@gmail.com>
2024-10-28 18:30:18 +0100

    seconda lezione

--- src/app/intestazione/intestazione.component.ts
+++ src/app/intestazione/intestazione.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { VoceMenu } from '../models/voce-menu';
 
 @Component({
   selector: 'app-intestazione',
   templateUrl: './intestazione.component.html',
-  styleUrl: './intestazione.component.css'
+  styleUrl: './intestazione.component.css',
 })
 export class IntestazioneComponent {
+  // links è la PROPRIETA' DELL'OGGETTO CHE VIENE INSTANZIATO DA IntestazioneComponent
 
+  vociMenu: VoceMenu[] = [
+    {
+      titolo: 'Chi siamo',
+      url: 'https://google.com',
+    },
+    {
+      titolo: 'Contatti',
+      url: 'https://instagram.com',
+    },
+    {
+      titolo: 'Assistenza',
+      url: 'https://ferrari.com',
+    },
+    {
+      titolo: 'Lavora per noi',
+      url: 'https://amazon.com',
+    },
+  ];
+
+  utenteLoggato = false;
 }
