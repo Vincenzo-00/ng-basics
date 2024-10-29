45e38fc07c4fd000de5c7b5b1fc2081c3736919f
Vincenzo-00 <vince.vasta18@gmail.com>
2024-10-29 18:47:09 +0100

    terza-lezione-seconda-parte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { Prodotto } from './models/prodotto';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ng-basics';
+
+  prodotti: Prodotto[] = [
+    {
+      nome: 'Bici',
+      desc: 'Nera',
+      prezzo: 100,
+      foto: 'https://picsum.photos/100/200',
+    },
+    {
+      nome: 'Frigittrice',
+      desc: 'Ad aria',
+      prezzo: 80,
+      foto: 'https://picsum.photos/100/200',
+    },
+  ];
+
+  prodottiFestaDellaDonna: Prodotto[] = [
+    {
+      nome: 'Scarpe',
+      desc: 'Nere',
+      prezzo: 150,
+      foto: 'https://picsum.photos/100/200',
+    },
+    {
+      nome: 'Borsa',
+      desc: 'Louis Vuitton',
+      prezzo: 800,
+      foto: 'https://picsum.photos/100/200',
+    },
+  ];
 }
--- src/app/gruppo-prodotti/gruppo-prodotti.component.ts
+++ src/app/gruppo-prodotti/gruppo-prodotti.component.ts
@@ -1,35 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Prodotto } from '../models/prodotto';
 
 @Component({
   selector: 'app-gruppo-prodotti',
   templateUrl: './gruppo-prodotti.component.html',
-  styleUrl: './gruppo-prodotti.component.css'
+  styleUrl: './gruppo-prodotti.component.css',
 })
-
 export class GruppoProdottiComponent {
+  @Input()
+  titolo: string = ''; // type inference
 
-  titolo: string = "Oggi in sconto!!!";
-
-  prodotti: Prodotto[] = [
-
-    {
-
-      nome: "Bici",
-      desc: "Nera",
-      prezzo: 100,
-      foto: "https://picsum.photos/100/200"
-
-    },
-    {
-
-      nome: "Frigittrice",
-      desc: "Ad aria",
-      prezzo: 80,
-      foto: "https://picsum.photos/100/200"
-
-    }
-
-  ]
-
+  @Input()
+  prodotti: Prodotto[] = [];
 }
